298baf9949370d74907e45999dc1879c42171bc9
hyun2y00 <pottq577@naver.com>
2025-05-07 17:32:08 +0900

    feat(todo): [1-3] 할 일 검색 시 검색 조건 요청 DTO 추가

---
+++ src/main/java/org/example/expert/domain/todo/dto/request/TodoFindRequest.java
@@ -0,0 +1,12 @@
+package org.example.expert.domain.todo.dto.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class TodoFindRequest {
+
+    private final String weather;
+    private final String modifiedAt;
+}
