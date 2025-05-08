053d01089035fc792f9f4cdcb5b608e15916b970
hyun2y00 <pottq577@naver.com>
2025-05-08 20:33:56 +0900

    refactor(todo): [2-3] RequiredArgsConstructor 롬복 사용 및 기존 생성자 제거

--- src/main/java/org/example/expert/domain/todo/repository/TodoQueryRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoQueryRepository.java
@@ -2,27 +2,24 @@ package org.example.expert.domain.todo.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.example.expert.domain.todo.entity.QTodo;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.user.entity.QUser;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class TodoQueryRepository {
 
     private final JPAQueryFactory queryFactory;
     private final QTodo qTodo = QTodo.todo;
     private final QUser qUser = QUser.user;
 
-    public TodoQueryRepository(final JPAQueryFactory queryFactory) {
-        this.queryFactory = queryFactory;
-    }
-
     public Optional<Todo> findByIdWithUser(long todoId) {
         return Optional.ofNullable(queryFactory
             .selectFrom(qTodo)
-            .leftJoin(qTodo.user, qUser)
-            .fetchJoin()
+            .leftJoin(qTodo.user, qUser).fetchJoin()
             .where(qTodo.id.eq(todoId))
             .fetchOne());
     }
